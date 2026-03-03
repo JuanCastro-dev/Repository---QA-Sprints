7c0a545591fad1d3e509f54329f08778ac42d9f4
Juan Castro <juan.mendonca@aircompany.ai>
2026-03-03 10:47:43 -0300

    /feat: Criada a classe calculadora com seus métodos

---
+++ Documentos/Fase 2/Sprint 1/Semana 2/Atividade Calculadora/calculadora.py
@@ -0,0 +1,33 @@
+class Calculadora:
+    def __init__(self,n1,n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def soma(self,n1,n2):
+        return n1+n2
+
+    def sub(self,n1,n2):
+        return n1-n2
+
+    def mul(self,n1,n2):
+        return n1*n2
+
+    def div(self,n1,n2):
+        if n2 == 0:
+            raise ZeroDivisionError("Impossível dividir um número por zero.")
+        return n1/n2
+
+    def resto(self,n1,n2):
+        if n2 == 0:
+            raise ZeroDivisionError("Impossível dividir um número por zero"
+                                    ", portanto não há resto")
+        return n1%n2
+
+    def potencia(self,n1,n2):
+        return n1**n2
+
+    def raiz(self,n1,n2):
+        if n2 == 0:
+            raise ValueError("Não é possível tirar a raiz zero de um número")
+        return n1**(1/n2)
+
